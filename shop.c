797d4cf563255e6a57eef0a89bf9a72fce478c46
DamienLeMalETPA <59608514+DamienLeMalETPA@users.noreply.github.com>
2020-01-29 18:31:48 +0100

    Shop display and sorting

--- shop.c
+++ shop.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int compteur = 1;
+int compteur = 2;
 
 struct Item {
 	int id;
@@ -10,6 +10,36 @@ struct Item {
 	char nom[99];
 };
 typedef struct Item item;
+
+
+void sort(item * varItem,item * varItem2){
+	int result = 0;
+	int holder = 0;
+	char holder_c[1][99];
+	result = strncmp((*varItem).nom,(*varItem2).nom,20);
+	if(result > 0) {
+      printf("str1 > str2");
+      holder = (*varItem).id;
+      (*varItem).id = (*varItem2).id;
+      (*varItem2).id = holder;
+
+      holder = (*varItem).prix;
+      (*varItem).prix = (*varItem2).prix;
+      (*varItem2).prix = holder;
+
+      holder = (*varItem).qqte_inventaire;
+      (*varItem).qqte_inventaire = (*varItem2).qqte_inventaire;
+      (*varItem2).qqte_inventaire = holder;
+
+      strcpy(holder_c[1],(*varItem).nom);
+      strcpy((*varItem).nom,(*varItem2).nom);
+      strcpy((*varItem2).nom,holder_c[1]);
+   } else if(result < 0) {
+      printf("str1 < str2");
+   } else {
+      printf("str1 = str2");
+   }
+}
 
 void addItem(item * varItem){
 
@@ -38,22 +68,46 @@ int main(){
 	int exit = 0;
 	int choix = 0;
 	//definition des structures de bases
-	item chocolat = {1,10,0,{"Chocolat"}};
+	item article[99];
+	article[0].id = 1;
+	article[0].prix = 10;
+	article[0].qqte_inventaire = 0;
+	strcpy(article[0].nom,"Chocolat");
+
+	article[1].id = 2;
+	article[1].prix = 20;
+	article[1].qqte_inventaire = 0;
+	strcpy(article[1].nom,"Banane");
+
 	char prenom[99][99];
 
-	strcpy(prenom[chocolat.id],chocolat.nom);
+	strcpy(prenom[article[0].id],article[0].nom);
+	strcpy(prenom[article[1].id],article[1].nom);
 	while (exit == 0){
 		printf("Menu :\n[1] Inventaire \n[2] Magasin \n[3] Nouvel Item dans le magasin ! \n[4] Quitter");
 		scanf("%d",&choix);
 		switch (choix){
 			case 1:
-				printf("%s\n",chocolat.nom);
+				printf("%s\n",article[0].nom);
 				break;
 			case 2:
-				printf("___________________________\n          Magasin          \n___________________________\n");
-				display_shop(&chocolat);
-				//boucle display
+				//sorting
+				while (test == 0){
+					test = 0;
+					for (int i = 0; i < compteur; ++i){
+				
 
+
+					}
+				}
+
+
+				printf("___________________________\n          Magasin          \n___________________________\n");
+				
+				//boucle display
+				for (int i = 0; i < compteur; ++i){
+					display_shop(&article[i]);
+				}
 
 				//achat
 				scanf("%d",&choix);
